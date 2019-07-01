af1150b5e3af8a9687b6e99915b355c3eb6af8f5
Sergey Morozov <sergey@morozov.ch>
2019-07-01 12:13:25 +0300

    fix licensing

--- include/state_vector_views.hpp
+++ include/state_vector_views.hpp
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2018-2019  Sergey Morozov <sergey@morozov.ch>
+ * Copyright (C) 2019  Sergey Morozov <sergey@morozov.ch>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
